28346a571af6d661fa1e491e3d85072789910568
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-03-28 14:59:43 +0100

    new2

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,14 +1,9 @@
-"use client";
 import React from "react";
-import Link from "next/link";
-import { motion } from "motion/react";
-import { HeroParallax } from "@/components/HeroParallax";
-import { Portfolio } from "@/data/data";
 
 const page = () => {
   return (
     <div className=" max-w-screen bg-background overflow-hidden">
-      <div className="pb-[10rem]">
+      {/* <div className="pb-[10rem]">
         <HeroParallax />
       </div>
 
@@ -23,7 +18,7 @@ const page = () => {
             className="flex flex-col w-full  justify-center items-center "
           >
             <div className="w-[30rem] text-left flex flex-col gap-6 pb-10">
-              {/* <Link href={{pathname:`blog/${item.id}`}} > */}
+              {/* <Link href={{pathname:`blog/${item.id}`}} > 
               <Link href={item.link}>
                 <motion.img
                   initial={{ scale: 1 }}
@@ -35,7 +30,7 @@ const page = () => {
                 />
                 <h1 className="text-4xl font-bold pt-10">{item.title}</h1>
               </Link>
-              {/* <hr className='border ml-[-2.5rem] border-white' /> */}
+              
               <p className=" font-semibold text-slate-800  text-[0.9rem] w-full">
                 {item.desc}
               </p>
@@ -54,7 +49,7 @@ const page = () => {
             <hr className="borde border-white" />
           </motion.div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
